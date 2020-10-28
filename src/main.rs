9758802082c1b0e0173f4f738049cae971adc0c5
adqn <57331120+adqn@users.noreply.github.com>
2020-10-28 10:31:58 -0700

    initialization

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,9 @@ use vulkano::device::DeviceExtensions;
 use vulkano::device::Features;
 use vulkano::buffer::BufferUsage;
 use vulkano::buffer::CpuAccessibleBuffer;
+use vulkano::command_buffer::AutoCommandBufferBuilder;
+use vulkano::command_buffer::CommandBuffer;
+use vulkano::sync::GpuFuture;
 
 fn main() {
     let instance = Instance::new(None, &InstanceExtensions::none(), None)
@@ -35,6 +38,16 @@ fn main() {
                                      source_content).expect("Failed to create buffer");
     let dest = CpuAccessibleBuffer::from_iter(device.clone(), BufferUsage::all(), false,
                                    dest_content).expect("Failed to create buffer");
+    
+    let mut builder = AutoCommandBufferBuilder::new(device.clone(), queue.family()).unwrap();
+    builder.copy_buffer(source.clone(), dest.clone()).unwrap();
+    let command_buffer = builder.build().unwrap();
+    let finished = command_buffer.execute(queue.clone()).unwrap();
 
-
+    finished.then_signal_fence_and_flush().unwrap()
+        .wait(None).unwrap();
+   
+    let src_content = source.read().unwrap();
+    let dest_content = dest.read().unwrap();
+    assert_eq!(&*src_content, &*dest_content);
 }
